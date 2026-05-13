74f2004dc6997afec9811f177bfe5c930be663a5
Gautam <gautamsaravanan6963@gmail.com>
2026-05-13 05:49:36 +0530

    deleted git practice files

--- src/API/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const Api=axios.create({
-    baseURL:"http://3.106.195.82/expresscinema/",
-    headers: {
-        "Content-Type": "application/json"
-    }
-})
-export default Api
--- src/service/MovieService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import Api from "../API/Api";
-
-const addMovie = (movie) => {
-  return Api.post("/saveMovie", movie);
-};
-
-const MovieService = { addMovie };
-export default MovieService;
